6da0b511471dc30c257168953966368aabd226ed
Satyajit Das <satyajitdrkl2006@gmail.com>
2025-12-26 17:01:27 +0530

    hero section

--- app.js
+++ app.js
@@ -16,4 +16,56 @@ label.addEventListener("click",()=>{
       
     };
     x++;
+
 })
+
+let index=0;
+
+const slides = document.querySelectorAll(".slides img");
+let l=slides.length;
+
+const nextBtn = document.querySelector(".right");
+const prevBtn = document.querySelector(".left");
+
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+//left
+prevBtn.addEventListener("click",()=>{
+    if(index<=0){
+        index=l-1;
+        showSlide(index);
+        console.log(index);
+        
+    }
+    else{
+    index--;
+    showSlide(index);
+    }
+    
+
+})
+function next(){
+   
+    if(index>=l-1){
+        index=0;
+        showSlide(index);
+        console.log(index);
+        
+    }else{
+        
+    index++;
+            console.log(index,"by else");
+
+    showSlide(index)}
+
+}
+nextBtn.addEventListener("click",()=>{
+    next();
+});
+
+// setInterval(next,6000);
+
